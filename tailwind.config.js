f4b967c42b54688413390f3acee4185351f84e28
FRanggaY <franciscusrangga@gmail.com>
2022-02-19 21:28:41 +0700

    setting flowbite and add custom extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      heights: {
+        '500': '500px',
+      },
+      width: {
+        '44': '44rem',
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
