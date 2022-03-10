8e5041f259d0e763207fd8a16c5e990b9d332069
Fadlan123-prog <fadlan.diyanuar12@gmail.com>
2022-03-10 12:11:55 +0700

    Merge branch 'francis-test' of https://github.com/FRanggaY/kpb-client into francis-test

--- src/components/modal/activity-modal/add-modal/index.jsx
+++ src/components/modal/activity-modal/add-modal/index.jsx
@@ -9,8 +9,8 @@ class ModalActivity extends Component {
                     <button class="block w-full md:w-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="large-modal">Tambah
                     </button>
                 </div>
-                <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="large-modal">
-                    <div class="relative px-4 w-full max-w-4xl h-full md:h-auto">
+                <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-8 z-50 justify-center items-center md:inset-0 h-modal sm:h-full" id="large-modal">
+                    <div class="relative px-4 w-full max-w-4xl h-full">
                         {/* <!-- Modal content --> */}
                         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                             {/* <!-- Modal header --> */}
