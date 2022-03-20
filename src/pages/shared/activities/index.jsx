b2f92f0f4d36b1f6f6dc9777d9dab7ad823aaad5
FRanggaY <franciscusrangga@gmail.com>
2022-03-20 18:15:16 +0700

    update activities

--- src/pages/shared/activities/index.jsx
+++ src/pages/shared/activities/index.jsx
@@ -3,10 +3,13 @@ import NavbarComponent from '../../../components/navbar';
 import FooterComponent from '../../../components/footer';
 import axios from 'axios';
 import moment from 'moment';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 class Activities extends Component {
     state = {
         activity: [],
+        date: new Date(),
     }
 
     componentDidMount() {
@@ -22,6 +25,10 @@ class Activities extends Component {
             ;
         });    
     }
+
+
+    onChange = date => this.setState({ date })
+
     render() {
         return (
             <Fragment>
@@ -30,35 +37,48 @@ class Activities extends Component {
                     <section className="relative py-5 mt-10">
                         <div className="container mx-auto px-5 lg:px-20">
                             <h6 className="text-left text-xl font-bold py-5">Kegiatan</h6>
-                            {this.state.activity.map((item, index) => {
-                            return(
-                                <div className="py-5">  
-                                <div class="blockbg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                                   <div className="flex flex-col lg:flex-row">
-                                       <div className="flex">
-                                            <div className="bg-gray-700 p-8 w-48 lg:rounded-l-lg rounded-tl-lg text-center">  
-                                                <h5 class="mb-2 text-2xl w-14 font-bold tracking-tight text-white">{moment(item.date).format('LL')}</h5>
-                                                {/* <h5 class="mb-2 text-2xl font-bold tracking-tight text-white"><span className="text-5xl">14</span> {item.created_at}</h5> */}
+                            <div className="flex lg:flex-col-reverse flex-col-reverse gap-5">
+                                {this.state.activity.map((item, index) => {
+                                return(
+                                    <div>
+                                        {moment(item.date).format('LL') === moment(this.state.date).format('LL') 
+                                        
+                                        ? 
+                                        <div class="blockbg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                            <div className="flex flex-col lg:flex-row">
+                                                <div className="p-8 w-72">  
+                                                    <img src={`http://localhost:8000/${item.image}`} />
+                                                </div>
+                                                <div className="p-8">
+                                                        <span class=" bg-blue-300 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 dark:bg-gray-700 dark:text-gray-300">
+                                                            <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
+                                                            {item.time}
+                                                        </span>
+                                                        <div>
+                                                            <a href="test" target="_blank" rel="noopener" class="mb-2 text-2xl font-bold ">{item.title}</a>
+                                                            <p class="font-normal text-gray-700 dark:text-gray-400">{item.description}</p>
+                                                        </div>
+                                                </div>
                                             </div>
-                                            <div className="p-8 w-72">  
-                                                <img src={`http://localhost:8000/${item.image}`} />
-                                            </div>
-                                       </div>
-                                       <div className="p-8">
-                                            <span class=" bg-blue-300 text-gray-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded mr-2 dark:bg-gray-700 dark:text-gray-300">
-                                                <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd"></path></svg>
-                                                {item.time}
-                                            </span>
-                                            <div>
-                                                <a href="test" target="_blank" rel="noopener" class="mb-2 text-2xl font-bold tracking-tight ">{item.title}</a>
-                                                <p class="font-normal text-gray-700 dark:text-gray-400">{item.description}</p>
-                                            </div>
-                                       </div>
-                                   </div>
+                                        </div>
+                                    
+                                        :
+                                        
+                                         null
+                                    
+                                        }   
+                                        
+                                    </div>
+                                )
+                                })}
+                                <div className="flex gap-5 lg:flex-row flex-col">
+                                    <Calendar
+                                        onChange={this.onChange}
+                                        value={this.state.date}
+                                    />
+                                    <p className="text-2xl font-bold text-black"> Date Select :  <span className="font-semibold">{moment(this.state.date.toString()).format('LL')}</span> </p>
                                 </div>
                             </div>
-                            )
-                            })}
                             
                         </div>
                         </section>
